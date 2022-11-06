76befa5aa4a498cd31dd4cdadddde30cb93485a6
ngkeanchewet <ngkeanchewet@gmail.com>
2022-11-06 20:38:54 +0800

    update plot

--- plot.py
+++ plot.py
@@ -35,10 +35,44 @@ def create_plot(df):
         secondary_y=False,
     )
 
+    for i, dd in enumerate(fig.data):
+        x, y = dd.x[-1], dd.y[-1]
+        if i == 0:
+            fig.add_scatter(
+                x=[x],
+                y=[y],
+                mode="markers+text",
+                text="{:.2f}".format(dd.y[-1]),
+                textfont=dict(color=dd.line.color),
+                textposition="top right",
+                marker=dict(color=dd.line.color, size=10, opacity=0.8),
+                legendgroup=dd.name,
+                showlegend=False,
+                secondary_y=True,
+            )
+
+        else:
+            x1, y1 = fig.data[i - 1].x[-1], fig.data[i - 1].y[-1]
+            if abs(y - y1) < 2:
+                y = y + 1
+
+            fig.add_scatter(
+                x=[x],
+                y=[y],
+                mode="markers+text",
+                text="{:.2f}%".format(dd.y[-1]),
+                textfont=dict(color=dd.line.color),
+                textposition="top right",
+                marker=dict(color=dd.line.color, size=12, opacity=0),
+                legendgroup=dd.name,
+                showlegend=False,
+                secondary_y=False,
+            )
+
     fig.update_layout(
         title=dict(text="SPY - Bond Yield (2Y & 10Y) - FED Rate", font_size=16),
-        width=1600,
-        height=800,
+        width=1200,
+        height=600,
         xaxis=dict(
             autorange=True,
             showline=True,
@@ -104,4 +138,3 @@ def create_plot(df):
         plot_bgcolor="white",
     )
     return fig
-
